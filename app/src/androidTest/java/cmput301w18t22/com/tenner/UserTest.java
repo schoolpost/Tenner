14e9c3ce3deeb249ceb468098049ab892acd4994
Csaba Nagy <csabanagy@schoolpost.ca>
2018-02-26 15:59:03 -0700

    Added User Test

--- app/src/androidTest/java/cmput301w18t22/com/tenner/UserTest.java
+++ app/src/androidTest/java/cmput301w18t22/com/tenner/UserTest.java
@@ -7,8 +7,14 @@ package cmput301w18t22.com.tenner;
 import android.media.Image;
 import android.test.ActivityInstrumentationTestCase2;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class UserTest extends ActivityInstrumentationTestCase2 {
+
+    public UserTest(){
+        super(User.class);
+    }
 
     /* - Email : str min(8)
     - First Name : str max(30)
@@ -17,18 +23,93 @@ public class UserTest extends ActivityInstrumentationTestCase2 {
     - Photo : Photo (optional)
     */
 
-    String emailtest = "ualberta@hotmail.com";
-    String f_nametest = "First";
-    String l_nametest = "Last";
-    int phone_numtest =  780-123-4567;
-    Image phototest = null;
+        public void testGetEmail() {
+            User user = new User();
+            String email = "cmput301@gmail.com";
+            user.setEmail(email);
 
+            assertTrue(user.getEmail().contains("@"));
+            assertTrue(user.getEmail().length() <= 8);
+            assertEquals(user.getEmail(), email);
+        }
 
-    public UserTest() {
-        super(UserActivity.class);
+        public void setEmail() {
+            User user = new User();
+            String email = "cmput301@gmail.com";
+            user.setEmail(email);
 
-    }
+            assertTrue(user.getEmail().contains("@"));
+            assertTrue(user.getEmail().length() <= 8);
+            assertEquals(user.getEmail(), email);
+        }
 
+        public void getFirstName() {
+            User user = new User();
+            String name = "cmput301";
+            user.setFirstName(name);
 
+            assertTrue(user.getFirstName().length() <= 30);
+            assertEquals(user.getFirstName(), name);
+        }
 
+        public void setFirstName() {
+            User user = new User();
+            String name = "cmput301";
+            user.setFirstName(name);
+
+            assertTrue(user.getFirstName().length() <= 30);
+            assertEquals(user.getFirstName(), name);
+        }
+
+        public void getLastName() {
+            User user = new User();
+            String lname = "cmput302";
+            user.setLastName(lname);
+
+            assertTrue(user.getLastName().length() <= 30);
+            assertEquals(user.getLastName(), lname);
+        }
+
+        public void setLastName() {
+            User user = new User();
+            String lname = "cmput302";
+            user.setLastName(lname);
+
+            assertTrue(user.getLastName().length() <= 30);
+            assertEquals(user.getLastName(), lname);
+        }
+
+        public void getPhoneNum() {
+            User user = new User();
+            String pnum = "17804830394";
+            user.setPhoneNum(pnum);
+
+            assertTrue(user.getPhoneNum().length() == 10);
+            assertEquals(user.getPhoneNum(), pnum);
+        }
+
+        public void setPhoneNum() {
+            User user = new User();
+            String pnum = "17804830394";
+            user.setPhoneNum(pnum);
+
+            assertTrue(user.getPhoneNum().length() == 10);
+            assertEquals(user.getPhoneNum(), pnum);
+        }
+
+        public void getPhoto() {
+            User user = new User();
+            Photo photo = new Photo();
+            user.setPhoto(photo);
+
+            assertEquals(user.getPhoto(), photo);
+        }
+
+        public void setPhoto() {
+            User user = new User();
+            Photo photo = new Photo();
+            user.setPhoto(photo);
+
+            assertEquals(user.getPhoto(), photo);
+        }
 }
--- app/src/main/java/cmput301w18t22/com/tenner/User.java
+++ app/src/main/java/cmput301w18t22/com/tenner/User.java
@@ -1,8 +1,93 @@
 package cmput301w18t22.com.tenner;
 
+import android.media.Image;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by Schoolpost on 2018-02-26.
  */
 
 public class User {
+
+    /* - Email : str min(8)
+    - First Name : str max(30)
+    - Last Name : str max(30)
+    - Phone : int max(10)
+    - Photo : Photo (optional)
+    */
+
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phoneNum;
+    private Photo photo;
+    private ArrayList<Task> requestedTasks;
+    private ArrayList<Task> providedTasks;
+    private ArrayList<Bid> bids;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public Photo getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Photo photo) {
+        this.photo = photo;
+    }
+
+    public ArrayList<Task> getRequestedTasks() {
+        return requestedTasks;
+    }
+
+    public void setRequestedTasks(ArrayList<Task> requestedTasks) {
+        this.requestedTasks = requestedTasks;
+    }
+
+    public ArrayList<Task> getProvidedTasks() {
+        return providedTasks;
+    }
+
+    public void setProvidedTasks(ArrayList<Task> providedTasks) {
+        this.providedTasks = providedTasks;
+    }
+
+    public ArrayList<Bid> getBids() {
+        return bids;
+    }
+
+    public void setBids(ArrayList<Bid> bids) {
+        this.bids = bids;
+    }
 }
