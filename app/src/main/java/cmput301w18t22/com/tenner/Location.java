d5c629ed7c6128805aadfc087368e833e1a7e951
cairnsdan <cairns@ualberta.ca>
2018-03-18 20:43:50 -0600

    Updated Javadoc; Tweaked Classes

--- app/src/main/java/cmput301w18t22/com/tenner/Location.java
+++ app/src/main/java/cmput301w18t22/com/tenner/Location.java
@@ -19,26 +19,59 @@ public class Location {
         this.address = address;
     }
 
+    /**
+     * @return the latitude of this location
+     * @see Location#setLatitude
+     */
     public float getLatitude() {
         return latitude;
     }
 
+
+    /**
+     * Set the latitude for this location.
+     *
+     * @param latitude Float value of latitude for location
+     */
     public void setLatitude(float latitude) {
         this.latitude = latitude;
     }
 
+
+    /**
+     * @return the longitude of this location
+     * @see Location#setLongitude
+     */
     public float getLongitude() {
         return longitude;
     }
 
+
+    /**
+     * Set the longitude for this location.
+     *
+     * @param longitude Float value of longitude for location
+     */
     public void setLongitude(float longitude) {
         this.longitude = longitude;
     }
 
+
+    /**
+     * @return String representing the address of this location
+     * @see Location#setAddress
+     */
     public String getAddress() {
         return address;
     }
 
+
+    /**
+     * Set the address for this location. Once we address the map use cases we will define limits.
+     *
+     *
+     * @param address String representing the address of this location
+     */
     public void setAddress(String address) {
         this.address = address;
     }
