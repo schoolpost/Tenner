1142e56c9c65e92cf8c6cdb9b918dbec7f1f6d26
Csaba Nagy <csabanagy@schoolpost.ca>
2018-03-19 16:00:58 -0600

    go

---
+++ app/build/generated/source/buildConfig/debug/cmput301w18t22/com/tenner/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cmput301w18t22.com.tenner;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "cmput301w18t22.com.tenner";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
