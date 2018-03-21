6a7934bd8a38abc9838490a170ffaaef62901642
EC2 Default User <ec2-user@ip-172-31-68-197.ec2.internal>
2018-03-21 21:47:13 +0000

    Add first endpoint

---
+++ app.js
@@ -0,0 +1,26 @@
+var express      = require('express'),
+    app          = express(),
+    session      = require('express-session'),
+    bodyParser   = require("body-parser");
+    
+//Configurations-------------------------------------------------------------->
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.use(session({
+    cookieName: 'session',
+    secret: "Secret Louis cmput 301",
+    resave: false,
+    saveUninitialized: false,
+    duration: 30 * 60 * 1000,
+    activeDuration: 5 * 60 * 1000,
+    secure : true
+}));
+
+app.get('/', function(request, response){
+    response.end('CMPUT 301 Tenner Served Started'); 
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Tenner Server Started!");
+});
