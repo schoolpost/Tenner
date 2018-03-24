17cdd6f00fe22e1f3322dfb686c3c1865b4bac4c
Louis Moc <moc@ualberta.ca>
2018-03-24 22:20:46 +0000

    update signupUser8

--- routes/routes.js
+++ routes/routes.js
@@ -50,7 +50,7 @@ router.get('/getAllUsers', function(request, response){
 router.post('/signUpUser', function(request, response){
     var user = request.body.user;
     console.log('lol');
-    console.log(request.body);
+    console.log(user);
     client.search({
       index: 'tenner',
       type: 'users'
