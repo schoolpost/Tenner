91c19745fe70745d777418f919137bd5bbc79657
Louis Moc <moc@ualberta.ca>
2018-03-30 23:52:02 +0000

    added getallusers

--- routes/routes.js
+++ routes/routes.js
@@ -211,6 +211,26 @@ router.get('/editUser', function(request, response){
     }
 });
 
+router.get('/getAllUsers', function(request, response){
+    client.search({
+      index: 'tenner',
+      type: 'users'
+      /*body: {
+        query: {
+          match: {
+            body:  ''
+          }
+        }
+      }*/
+    }).then(function (responseBody) {
+        var data = responseBody.hits.hits;
+        return response.send(data);
+    }, function (err) {
+        console.log(err.message);
+        return response.send({'Error' : 'At /getAllUsers ' + err.message});
+    });
+});
+
 //Tasks-------------------------------------------------------------->
 
 router.post('/getAssignedTasks', function(request, response){
