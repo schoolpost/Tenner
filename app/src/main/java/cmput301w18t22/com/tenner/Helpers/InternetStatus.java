4d43c708cace7af0ec4e14f9672f13ba786e93ad
Louis <louismc4@hotmail.com>
2018-04-05 17:38:37 -0600

    internetstatus and change status class

--- app/src/main/java/cmput301w18t22/com/tenner/Helpers/InternetStatus.java
+++ app/src/main/java/cmput301w18t22/com/tenner/Helpers/InternetStatus.java
@@ -1,6 +1,13 @@
 package cmput301w18t22.com.tenner.Helpers;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 class InternetStatus {
+
+    //https://developer.android.com/training/monitoring-device-state/connectivity-monitoring.html#DetermineType
+
     private static final InternetStatus ourInstance = new InternetStatus();
 
     static InternetStatus getInstance() {
@@ -8,5 +15,17 @@ class InternetStatus {
     }
 
     public InternetStatus() {
+    }
+
+    public boolean isConnected(Context ct) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) ct.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo currentActiveNetwork = connectivityManager.getActiveNetworkInfo();
+
+        if(currentActiveNetwork != null && currentActiveNetwork.isConnectedOrConnecting()){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
