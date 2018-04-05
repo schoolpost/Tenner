e882816199e997b56f47f7c7cca547cfdb86faf8
Yu Kin Law <yklaw@ualberta.ca>
2018-04-05 08:10:49 -0600

    EditProfile saves changes

--- app/src/main/java/cmput301w18t22/com/tenner/ui/activity/EditProfileActivity.java
+++ app/src/main/java/cmput301w18t22/com/tenner/ui/activity/EditProfileActivity.java
@@ -1,16 +1,21 @@
 package cmput301w18t22.com.tenner.ui.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import cmput301w18t22.com.tenner.R;
+import cmput301w18t22.com.tenner.broadcast.BroadcastManager;
+import cmput301w18t22.com.tenner.classes.User;
+import cmput301w18t22.com.tenner.server.ElasticSearchRestClient;
+import cmput301w18t22.com.tenner.utils.Authenticator;
+import cmput301w18t22.com.tenner.utils.LocalDataHandler;
+import cmput301w18t22.com.tenner.utils.SharedPrefUtils;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -19,6 +24,9 @@ public class EditProfileActivity extends AppCompatActivity {
     private EditText etLast;
     private EditText etPhone;
     private TextView save;
+
+    private User user;
+    private LocalDataHandler localDataHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +44,7 @@ public class EditProfileActivity extends AppCompatActivity {
         etLast = (EditText) findViewById(R.id.edit_lastname);
         etPhone = (EditText) findViewById(R.id.edit_phone);
 
+        loadData();
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,20 +53,68 @@ public class EditProfileActivity extends AppCompatActivity {
             }
         });
 
-        // Get the value from the server
-        tvEmail.setText("test@gmail.com");
-        etFirst.setText("Csaba");
-        etLast.setText("Nagy");
-        etPhone.setText("XXX-XXX-XXXX");
 
     }
 
-    public void checkChanges() {
-        // Check for inputs against constraints
+    private void loadData() {
+        user = localDataHandler.loadUserFromFile();
+
+        etFirst.setText(user.getFirstName());
+        etLast.setText(user.getLastName());
+        tvEmail.setText(user.getEmail());
+        etPhone.setText(user.toDisplayPhone());
+    }
+
+    boolean check(String first, String last, String phone) {
+        Authenticator a = new Authenticator();
+
+        if (!a.checkName(first)) {
+            etFirst.setError("invalid first name");
+            return false;
+        }
+        if (!a.checkName(last)) {
+            etLast.setError("invalid last name");
+            return false;
+        }
+        if (!a.checkPhoneNum(phone)) {
+            etPhone.setError("invalid phone number");
+            return false;
+        }
+        return true;
     }
 
     public void applyChanges() {
-        // Save to profile
+
+        String firstName = String.valueOf(etFirst.getText()).trim();
+        String lastName = String.valueOf(etLast.getText()).trim();
+        String phone = String.valueOf(etPhone.getText()).trim();
+
+        if (check(firstName, lastName, phone)) {
+            if (TextUtils.isEmpty(firstName)) {
+                firstName = user.getFirstName();
+            }
+            if (TextUtils.isEmpty(lastName)) {
+                lastName = user.getLastName();
+            }
+            if (TextUtils.isEmpty(firstName)) {
+                phone = user.toDisplayPhone();
+            }
+
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+            user.setPhoneNum(phone);
+        }
+
+        ElasticSearchRestClient elasticSearchRestClient = ElasticSearchRestClient.getInstance();
+        try {
+            elasticSearchRestClient.postUser(user);
+        } catch (Exception e) {
+
+        }
+
+        SharedPrefUtils.login(this, "");
+        BroadcastManager.sendLoginBroadcast(this, 1);
+        localDataHandler.saveUserInFile(user);
         finish(); // Back to profile fragment with updated values
     }
 
