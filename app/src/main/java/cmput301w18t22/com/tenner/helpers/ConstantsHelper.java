a54d10b5a61740b592b9a6b766787e53bbac61ec
Csaba Nagy <csabanagy@schoolpost.ca>
2018-04-07 15:25:22 -0600

    Localdata helper for load/save tasks

--- app/src/main/java/cmput301w18t22/com/tenner/helpers/ConstantsHelper.java
+++ app/src/main/java/cmput301w18t22/com/tenner/helpers/ConstantsHelper.java
@@ -6,7 +6,8 @@ package cmput301w18t22.com.tenner.helpers;
 
 public class ConstantsHelper {
     public static final String USERFILE = "user.sav";
-    public static final String TASKFILE  = "tasks.sav";
+    public static final String RTASKSFILE = "requestedTasks.sav";
+    public static final String PTASKSFILE = "provdidingTasks.sav";
     public static final int EDIT_PROFILE_REQUEST = 45;
     public static final int EDIT_PROFILE_RESULT = 45;
     public static final int ADD_TASK_REQUEST = 60;
--- app/src/main/java/cmput301w18t22/com/tenner/helpers/LocalDataHelper.java
+++ app/src/main/java/cmput301w18t22/com/tenner/helpers/LocalDataHelper.java
@@ -1,8 +1,10 @@
 package cmput301w18t22.com.tenner.helpers;
 
-import android.app.Activity;
+import
+        android.app.Activity;
 import android.content.Context;
 
+import com.google.android.gms.tasks.Tasks;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -15,7 +17,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 
+import cmput301w18t22.com.tenner.classes.Task;
 import cmput301w18t22.com.tenner.classes.User;
 
 /**
@@ -71,6 +75,83 @@ public class LocalDataHelper {
             throw new RuntimeException();
         }
 
+    }
+
+    public void saveRequestedTasksToFile(ArrayList<Task> tasks) {
+        try {
+            FileOutputStream fos = activity.openFileOutput(ConstantsHelper.RTASKSFILE,
+                    Context.MODE_PRIVATE);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+
+
+            Gson gson = new Gson();
+            gson.toJson(tasks, out);
+            out.flush();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void saveProvidingTasksToFile(ArrayList<Task> tasks) {
+        try {
+            FileOutputStream fos = activity.openFileOutput(ConstantsHelper.PTASKSFILE,
+                    Context.MODE_PRIVATE);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+
+
+            Gson gson = new Gson();
+            gson.toJson(tasks, out);
+            out.flush();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public ArrayList<Task> getRequestedTasks() {
+        try {
+            FileInputStream fis = activity.openFileInput(ConstantsHelper.RTASKSFILE);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Gson gson = new Gson();
+
+            Type fileType = new TypeToken<ArrayList<Task>>() {
+            }.getType();
+            ArrayList<Task> tasks = gson.fromJson(in, fileType);
+            return tasks;
+
+        } catch (FileNotFoundException e) {
+            return new ArrayList<Task>();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public ArrayList<Task> getProvidingTasks() {
+        try {
+            FileInputStream fis = activity.openFileInput(ConstantsHelper.PTASKSFILE);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Gson gson = new Gson();
+
+            Type fileType = new TypeToken<ArrayList<Task>>(){}.getType();
+            ArrayList<Task> tasks = gson.fromJson(in, fileType);
+            return tasks;
+
+        } catch (FileNotFoundException e) {
+            return new ArrayList<Task>();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
     }
 
 }
