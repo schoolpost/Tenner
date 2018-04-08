1ef45cf9b1ec55fc96656a4399ccbc0a84df9767
Louis Moc <moc@ualberta.ca>
2018-04-08 01:34:08 +0000

    logging delete task 2

--- routes/routes.js
+++ routes/routes.js
@@ -337,7 +337,7 @@ router.post('/getProvidingTasks', function(request, response){
 });
 
 ///???
-router.get('/deleteTask', function(request, response){
+router.post('/deleteTask', function(request, response){
     var user = request.body.user;
     var title = request.body.title;
     console.log(title + user);
