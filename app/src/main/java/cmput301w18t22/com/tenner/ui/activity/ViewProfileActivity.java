09c2418d262bf95e11b62f2bf99c97ebb1ffe7eb
yklaw <yklaw@ualberta.ca>
2018-04-08 14:26:24 -0600

    ViewProfile Changes

--- app/src/main/java/cmput301w18t22/com/tenner/ui/activity/ViewProfileActivity.java
+++ app/src/main/java/cmput301w18t22/com/tenner/ui/activity/ViewProfileActivity.java
@@ -1,8 +1,5 @@
 package cmput301w18t22.com.tenner.ui.activity;
 
-import android.graphics.Bitmap;
-import android.os.Handler;
-import android.os.Looper;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,7 +11,6 @@ import cmput301w18t22.com.tenner.classes.Task;
 import cmput301w18t22.com.tenner.classes.User;
 import cmput301w18t22.com.tenner.helpers.LocalDataHelper;
 import cmput301w18t22.com.tenner.helpers.PhotoConverterHelper;
-import cmput301w18t22.com.tenner.ui.fragment.ProfileFragment;
 
 public class ViewProfileActivity extends AppCompatActivity {
 
@@ -39,23 +35,22 @@ public class ViewProfileActivity extends AppCompatActivity {
         getSupportActionBar().setElevation(3);
         getSupportActionBar().setCustomView(R.layout.toolbar_home);
         pageTitle = (getSupportActionBar().getCustomView().findViewById(R.id.home_action_bar_title));
-        pageTitle.setText("User Profile");
+        pageTitle.setText("Requester Profile");
 
         name = (TextView) findViewById(R.id.view_profile_name);
         email = (TextView) findViewById(R.id.view_profile_email);
         phone = (TextView) findViewById(R.id.view_profile_phone);
         profilePic = (ImageView) findViewById(R.id.view_profile_picture);
 
-
         loadFromFile();
     }
 
-
-
     public void loadFromFile(){
+        // Load Requester profile from task
         task = localDataHelper.getTaskFromFile();
         user = task.getRequester();
-
+        
+        // Set the value to display
         name.setText(user.toProfileName()); // First + Last Name
         email.setText(user.getEmail());
         phone.setText(user.toDisplayPhone());
