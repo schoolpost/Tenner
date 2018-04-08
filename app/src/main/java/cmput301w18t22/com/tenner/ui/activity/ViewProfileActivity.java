c506ec1f1fad6a838763baae3872f1b89944414b
yklaw <yklaw@ualberta.ca>
2018-04-08 13:38:04 -0600

    Recreated ViewProfileActivity

---
+++ app/src/main/java/cmput301w18t22/com/tenner/ui/activity/ViewProfileActivity.java
@@ -0,0 +1,15 @@
+package cmput301w18t22.com.tenner.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cmput301w18t22.com.tenner.R;
+
+public class ViewProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_profile);
+    }
+}
